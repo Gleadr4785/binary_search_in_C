e0a25fce802883a14406eb3573f6e6d25ca7cd2b
janK <jan.ksenak@gmail.com>
2020-10-20 09:30:04 +0200

    final final version of this solution

--- list_calculator.c
+++ list_calculator.c
@@ -1,3 +1,14 @@
+//Given a list of integers nums, a string op representing either "+", "-",
+// "/", or "*", and an integer val, perform the operation on every 
+// number in nums with val and return the result.
+//Example 1
+//Input 
+//  nums = [3, 1, 6]
+//  op = "+"
+//  val = 4
+//Output
+//  [7,5,11]
+
 #include <stdio.h>
 
 int* list_calculator(int nums[], char op[2], int val);
@@ -5,9 +16,15 @@ int* list_calculator(int nums[], char op[2], int val);
 int main(void){
 
     int nums[]={1,2,3};
-    list_calculator(nums,"+",2);
+    list_calculator(nums,"/",0);
 
-    return 0;
+    printf("[ ");
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d ", nums[i]);
+        
+    }
+    printf("]");
 }
 
 int* list_calculator(int nums[], char op[2], int val){
@@ -26,14 +43,14 @@ int* list_calculator(int nums[], char op[2], int val){
                 nums[i]*=val;
             }
         }
-        if(op =="/" && val!=0){
+        if(op =="/"){
+            if (val == 0){
+                printf("\nYou cant divide by zero\n");
+            }
             for(int i=0;i<3;i++){
                 nums[i]/=val;
             }
         }
-        for (int i = 0; i < 3; i++)
-        {
-            
-        }
+        
         return nums;
     }
